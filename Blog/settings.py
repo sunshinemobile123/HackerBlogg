4c377ab0a32ab63c9809895c8931ce57d076422d
sunshinemobile123 <sunshinemobilesinamanagal@gmail.com>
2025-05-10 16:14:06 +0545

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -81,9 +81,7 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+import dj_database_url DATABASES = { 'default': dj_database_url.config(default='postgresql://blogg_7ex1_user:OCmnPrATrjRR4ZMrgkoDdfWcLy8sr31N@dpg-d0fij3juibrs73eo87qg-a/blogg_7ex1') }
 
 
 
