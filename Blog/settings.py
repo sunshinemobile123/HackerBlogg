1867ab36769b8a1cb7e6def398bf60680c34ef2c
sunshinemobile123 <sunshinemobilesinamanagal@gmail.com>
2025-05-10 16:06:45 +0545

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -82,11 +82,9 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 # Password validation
